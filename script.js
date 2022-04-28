9b3430bba3c90dc97dc2f653ad951c4c2cbdcf44
Ehsan A. Kian <ehsan_amd72@hotmail.com>
2022-04-28 20:05:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let count = 0
 function increment() {
     count = count + 1
-    document.getElementById("count-el").innerHTML = count
+    setCounter(count)
 }
 function decrement() {
-    if(count < 1) {
-        count = 0
-    } else {
+    if(count > 0) {
         count = count - 1
     }
-    document.getElementById("count-el").innerHTML = count
+    setCounter(count)
 }
 function reset() {
     count = 0
+    setCounter(count)
+}
+
+function setCounter(count) {
     document.getElementById("count-el").innerHTML = count
 }
